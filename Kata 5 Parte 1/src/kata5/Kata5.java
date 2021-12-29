8e703b629c5b53444f6817214a47e704f1bd45f2
MarkuzESP <marcosdiazgil@gmail.com>
2021-12-29 18:30:17 +0000

    Kata 5P1 Versión 2
    Creando nuevas tablas en la BD desde Java y comprobando en el SQLite Browser su correcta creación

--- Kata 5 Parte 1/src/kata5/Kata5.java
+++ Kata 5 Parte 1/src/kata5/Kata5.java
@@ -13,21 +13,20 @@ import java.sql.Statement;
 public class Kata5 {
     public static void main(String[] args){
         String url = "jdbc:sqlite:Prueba.db";
-        String sql = "SELECT * FROM PEOPLE";
+        // Es conveniente comprobar que una tabla NO exista antes de crearla
+        String sql = "CREATE TABLE IF NOT EXISTS Email (\n"
+                + " id integer PRIMARY KEY AUTOINCREMENT, \n"
+                + " correo text NOT NULL);";
+        
+        
         Connection conn;
         try{
             conn = DriverManager.getConnection(url);
-            System.out.println("Conectado a la base de datos");
+            System.out.println("Conectado a la base de datos.");
             Statement sentence = conn.createStatement();
-            ResultSet rs = sentence.executeQuery(sql);
-            
-            while (rs.next()){
-                System.out.println(rs.getInt("Id") + "\t" +
-                        rs.getString("Nombre") + "\t" +
-                        rs.getString("Apellidos") + "\t" +
-                        rs.getString("Depto") + "\t");
-            }
-            
+            sentence.execute(sql);
+            System.out.println("Tabla Email registrada.");
+                    
         } catch (SQLException e){
             System.out.println("Error al trabajar en la BD: " + e.getMessage());
         }
